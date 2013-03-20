b42af7bd8395504687548dfe2c8f003106fb0288
furyjeff1991@gmail.com <furyjeff1991@gmail.com@6216b847-7bda-ac2b-6cda-d94a60248162>
2013-03-20 07:14:37 +0000

    Added add edit delete for certain modules wala na talaga ako madagdag ahaha. Kulang pa tayo view sa JSP.

---
+++ trunk/JawsmithProgram/src/main/java/com/jawsmith/common/User.java
@@ -0,0 +1,36 @@
+package com.jawsmith.common;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.jawsmith.interfaces.DataAccesses;
+import com.jawsmith.interfaces.SystemUserMethods;
+import com.jawsmith.model.SystemUser;
+
+public class User
+{
+    public static void main( String[] args )
+    {
+    	ApplicationContext appContext = 
+    		new ClassPathXmlApplicationContext("spring/config/BeanLocations.xml");
+    	String[] test={"1","2","3"};
+    	SystemUserMethods sysMethods = (SystemUserMethods) appContext.getBean("systemUserBean");
+
+    	DataAccesses sysUserBo = (DataAccesses)appContext.getBean("systemUserBean");
+    	
+    	/**SystemUser sysUser = (SystemUser)sysMethods.findByName("apriljoy");
+    	sysUser.setPasswordChangeDate("10/22/12");
+    	sysUserBo.update(sysUser);
+    	 insert **/
+    	SystemUser user = new SystemUser();
+    	user.setAccess(1);
+    	user.setFirst_name("arnulfo");
+    	user.setLast_name("damo");
+    	user.setPassword("121212");
+    	user.setUsername("aj");
+  
+    	
+    	sysUserBo.save(user);
+    
+    }
+}
--- trunk/JawsmithProgram/src/main/java/com/jawsmith/controllers/PatientController.java
+++ trunk/JawsmithProgram/src/main/java/com/jawsmith/controllers/PatientController.java
@@ -33,16 +33,6 @@ public class PatientController {
 		ApplicationContext appContext = 
 			new ClassPathXmlApplicationContext("spring/config/BeanLocations.xml");
 		DataAccesses dataAccesses = (DataAccesses)appContext.getBean("patientsBean");
-	
-		@RequestMapping("/add_patient")
-		public String PatientView(HttpServletRequest request, HttpServletResponse response, 
-									  ModelMap model, Principal principal) throws IOException, ServletException{
-			
-			//Adding the list for the view
-			view(model);
-			
-			return "add_patient";
-		}
 		
 		public void view(ModelMap model){
 			ArrayList<Patient> list = new ArrayList<Patient>();
@@ -62,7 +52,7 @@ public class PatientController {
 		 * Method after finishing the add page
 		 * 
 		 **/
-		@RequestMapping("/patient/add/done")
+		@RequestMapping("/patient/add")
 		public String AddMethod(HttpServletRequest request, HttpServletResponse response, 
 										 ModelMap model, Principal principal) throws IOException, ServletException{
 		Patient patient = new Patient();
@@ -83,7 +73,6 @@ public class PatientController {
 			String patient_num = "1";
 			String nationality="Filipino";
 			Boolean status=Boolean.valueOf("1");
-			
 			Date dob = new Date();
 			Date last_visit_date= new Date();
 			
@@ -118,14 +107,63 @@ public class PatientController {
 		@RequestMapping("/patient/edit")
 		public String EditMethod(HttpServletRequest request, HttpServletResponse response, 
 		ModelMap model, Principal principal) throws IOException, ServletException{
-			return null;
+			
+			Patient patient = new Patient();
+			
+			int editId = Integer.parseInt(request.getParameter("editId"));
+			String last_name = request.getParameter("last_name");
+			String first_name = request.getParameter("first_name");
+			String middle_name = request.getParameter("middle_name");
+			String sex = request.getParameter("sex");
+			String rel_status = request.getParameter("rel_status");
+			String address = request.getParameter("address");
+			String city = "manila";
+			String tel_num = request.getParameter("tel_num");
+			String mobile_num = request.getParameter("mobile_num");
+			String email = request.getParameter("email");
+			String occupation = request.getParameter("occupation");
+			String religion = request.getParameter("religion");
+			String referred_by = request.getParameter("referred_by");
+			String guardian = request.getParameter("guardian");
+			String patient_num = "1";
+			String nationality="Filipino";
+			Boolean status=Boolean.valueOf("1");
+			Date dob = new Date();
+			Date last_visit_date= new Date();
+			
+			patient.setStatus(status);
+			patient.setLast_visit_date(last_visit_date);
+			patient.setNationality(nationality);
+			patient.setPatient_num(patient_num);
+			patient.setLast_name(last_name);
+			patient.setFirst_name(first_name);
+			patient.setMiddle_name(middle_name);
+			patient.setBirthday(dob);
+			patient.setSex(sex.charAt(0));
+			patient.setRelationship_status(rel_status);
+			patient.setAddress(address);
+			patient.setCity(city);
+			patient.setTelephone_number(tel_num);
+			patient.setMobile_number(mobile_num);
+			patient.setEmail_address(email);
+			patient.setOccupation(occupation);
+			patient.setReligion(religion);
+			patient.setReferred_by(referred_by);
+			patient.setGuardian(guardian);
+			patient.setPatient_id(editId);
+			
+			dataAccesses.update(patient);
+			return "home_page";
 		}
 		
-		
-		@RequestMapping("/patient/edit/done")
-		public String EditMethodDone(HttpServletRequest request, HttpServletResponse response, 
+		@RequestMapping("/patient/delete")
+		public String delete(HttpServletRequest request, HttpServletResponse response, 
 		ModelMap model, Principal principal) throws IOException, ServletException{
-			return null;
+			
+			int editId = Integer.parseInt(request.getParameter("editId"));
+			Patient patient = (Patient)dataAccesses.findById(editId);
+			patient.setStatus(Boolean.valueOf("0"));
+			return "home_page";	
 		}
 		
 		  @RequestMapping(value = "/patientGenerateReport") 
